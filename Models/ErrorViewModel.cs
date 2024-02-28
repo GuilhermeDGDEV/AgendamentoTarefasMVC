127f9b80e5e09693068a7d6d77f5de8912ae07a2
GuilhermeDGDEV <guilhermediasgoncalvesdev@gmail.com>
2024-02-28 06:54:42 -0300

    Criando o projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AgendamentoTarefasMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
